7dcde4cadd35c29bece423715376639e6b7849cb
OptimusIacobus <supajake118@gmail.com>
2018-04-07 02:42:22 -0400

    done almost

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,27 +1,20 @@
-// JS Source from https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
-// Note: also uses Jquery, so remember to double check that it is imported.
-
 $(document).ready(function () {
-    // Add smooth scrolling to all links
+
     $("a").on('click', function (event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
 
-            // Store hash
+        if (this.hash !== "") {
+
+            event.preventDefault();
+            
             var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function () {
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        } 
     });
 });
